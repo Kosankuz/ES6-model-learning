a829cd248e30d528abe619b3390fb4cfa90deb66
Slava <38960225+devcontract@users.noreply.github.com>
2018-08-15 00:59:11 +0300

    second_export
    second_export was declared  but not used!

--- module_test/var/main.js
+++ module_test/var/main.js
@@ -1,3 +1,13 @@
-import {first_export} from './export.js';
+import {
+  first_export
+} from './export.js';
+import {
+  second_export
+} from './export.js';
 
-console.log(first_export);
+console.log(first_export, second_export);
+if (first_export + second_export == 55) {
+  console.log("20 + 35 = 55 True");
+} else {
+  console.log("False");
+}
