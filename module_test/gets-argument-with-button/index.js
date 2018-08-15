4cfd234a84dbb0d49cc0900635083685d4579f3e
kosankuz <konstantinkuzmenko@icloud.com>
2018-08-15 22:03:23 +0100

    1

---
+++ module_test/gets-argument-with-button/index.js
@@ -0,0 +1,2 @@
+import {turn} from './export.js';
+document.getElementById('button').addEventListener('click', function(){document.getElementById('output').innerHTML = turn(34,45)});
--- module_test/gets_argument/gets-argument-with-button/index.js
+++
@@ -1,3 +0,0 @@
-import {turn} from './export.js';
-
- document.getElementById('button').addEventListener('click', function(){document.getElementById('output').innerHTML = turn(34,45)});
